6fd1453b86f12d965bbe5c68e92c84603140560c
hugochan <hugochan2013@gmail.com>
2017-01-18 17:13:32 -0500

    support cv

--- run_classifier.py
+++ run_classifier.py
@@ -9,6 +9,7 @@ import argparse
 import numpy as np
 from keras.utils import np_utils
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import ShuffleSplit
 
 from autoencoder.testing.classifier import neural_classifier
 from autoencoder.utils.io_utils import load_json, load_marshal
@@ -22,6 +23,7 @@ def main():
     parser.add_argument('-nv', '--n_val', type=int, default=1000, help='size of validation set (default 1000)')
     parser.add_argument('-ne', '--n_epoch', type=int, default=100, help='num of epoches (default 100)')
     parser.add_argument('-bs', '--batch_size', type=int, default=100, help='batch size (default 100)')
+    parser.add_argument('-cv', '--cross_validation', type=int, help='k-fold cross validation')
     args = parser.parse_args()
 
     # autoencoder
@@ -40,6 +42,8 @@ def main():
     # X_test = np.array(load_marshal(args.test_doc_codes))
     # Y_test = load_marshal(args.test_doc_labels)
 
+
+
     encoder = LabelEncoder()
     encoder.fit(Y_train)
     Y_train = np_utils.to_categorical(encoder.transform(Y_train))
@@ -47,16 +51,35 @@ def main():
 
     seed = 7
     np.random.seed(seed)
-    val_idx = np.random.choice(range(X_train.shape[0]), args.n_val, replace=False)
-    train_idx = list(set(range(X_train.shape[0])) - set(val_idx))
-    X_new_train = X_train[train_idx]
-    Y_new_train = Y_train[train_idx]
-    X_new_val = X_train[val_idx]
-    Y_new_val = Y_train[val_idx]
-    print 'train: %s, val: %s, test: %s' % (X_new_train.shape[0], X_new_val.shape[0], X_test.shape[0])
-    results = neural_classifier(X_new_train, Y_new_train, X_new_val, Y_new_val, \
-            X_test, Y_test, nb_epoch=args.n_epoch, batch_size=args.batch_size, seed=seed)
-    print 'acc on test set: %s' % results
+    if not args.cross_validation:
+        val_idx = np.random.choice(range(X_train.shape[0]), args.n_val, replace=False)
+        train_idx = list(set(range(X_train.shape[0])) - set(val_idx))
+        X_new_train = X_train[train_idx]
+        Y_new_train = Y_train[train_idx]
+        X_new_val = X_train[val_idx]
+        Y_new_val = Y_train[val_idx]
+        print 'train: %s, val: %s, test: %s' % (X_new_train.shape[0], X_new_val.shape[0], X_test.shape[0])
+        results = neural_classifier(X_new_train, Y_new_train, X_new_val, Y_new_val, \
+                X_test, Y_test, nb_epoch=args.n_epoch, batch_size=args.batch_size, seed=seed)
+        print 'acc on test set: %s' % results
+    else:
+        X = np.concatenate((X_train, X_test), axis=0)
+        Y = np.concatenate((Y_train, Y_test), axis=0)
+        ss = ShuffleSplit(n_splits=int(args.cross_validation), test_size=X_test.shape[0], random_state=seed)
+        results = []
+        for train_idx, test_idx in ss.split(X):
+            val_idx = np.random.choice(train_idx, args.n_val, replace=False)
+            new_train_idx = list(set(train_idx) - set(val_idx))
+            X_new_train = X[new_train_idx]
+            Y_new_train = Y[new_train_idx]
+            X_new_val = X[val_idx]
+            Y_new_val = Y[val_idx]
+            results.append(neural_classifier(X_new_train, Y_new_train, X_new_val, Y_new_val, \
+                X[test_idx], Y[test_idx], nb_epoch=args.n_epoch, batch_size=args.batch_size, seed=seed))
+
+        mean = np.mean(results)
+        std = np.std(results)
+        print 'acc on %s cross validation: %s (%s)' % (int(args.cross_validation), mean, std)
     import pdb;pdb.set_trace()
 
 if __name__ == '__main__':
--- run_regression.py
+++ run_regression.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import
 import argparse
 import numpy as np
 from keras.utils import np_utils
+from sklearn.model_selection import ShuffleSplit
 
 from autoencoder.testing.regression import neural_regression
 from autoencoder.utils.io_utils import load_json, load_marshal
@@ -21,6 +22,7 @@ def main():
     parser.add_argument('-nv', '--n_val', type=int, default=1000, help='size of validation set (default 1000)')
     parser.add_argument('-ne', '--n_epoch', type=int, default=100, help='num of epoches (default 100)')
     parser.add_argument('-bs', '--batch_size', type=int, default=100, help='batch size (default 100)')
+    parser.add_argument('-cv', '--cross_validation', type=int, help='k-fold cross validation')
     args = parser.parse_args()
 
     # autoencoder
@@ -41,17 +43,36 @@ def main():
 
     seed = 7
     np.random.seed(seed)
-    val_idx = np.random.choice(range(X_train.shape[0]), args.n_val, replace=False)
-    train_idx = list(set(range(X_train.shape[0])) - set(val_idx))
-    X_new_train = X_train[train_idx]
-    Y_new_train = Y_train[train_idx]
-    X_new_val = X_train[val_idx]
-    Y_new_val = Y_train[val_idx]
-    print 'train: %s, val: %s, test: %s' % (X_new_train.shape[0], X_new_val.shape[0], X_test.shape[0])
+    if not args.cross_validation:
+        val_idx = np.random.choice(range(X_train.shape[0]), args.n_val, replace=False)
+        train_idx = list(set(range(X_train.shape[0])) - set(val_idx))
+        X_new_train = X_train[train_idx]
+        Y_new_train = Y_train[train_idx]
+        X_new_val = X_train[val_idx]
+        Y_new_val = Y_train[val_idx]
+        print 'train: %s, val: %s, test: %s' % (X_new_train.shape[0], X_new_val.shape[0], X_test.shape[0])
 
-    results = neural_regression(X_new_train, Y_new_train, X_new_val, Y_new_val, \
-            X_test, Y_test, nb_epoch=args.n_epoch, batch_size=args.batch_size, seed=seed)
-    print 'r2 score on test set: %s' % results
+        results = neural_regression(X_new_train, Y_new_train, X_new_val, Y_new_val, \
+                X_test, Y_test, nb_epoch=args.n_epoch, batch_size=args.batch_size, seed=seed)
+        print 'r2 score on test set: %s' % results
+    else:
+        X = np.concatenate((X_train, X_test), axis=0)
+        Y = np.concatenate((Y_train, Y_test), axis=0)
+        ss = ShuffleSplit(n_splits=int(args.cross_validation), test_size=X_test.shape[0], random_state=seed)
+        results = []
+        for train_idx, test_idx in ss.split(X):
+            val_idx = np.random.choice(train_idx, args.n_val, replace=False)
+            new_train_idx = list(set(train_idx) - set(val_idx))
+            X_new_train = X[new_train_idx]
+            Y_new_train = Y[new_train_idx]
+            X_new_val = X[val_idx]
+            Y_new_val = Y[val_idx]
+            results.append(neural_regression(X_new_train, Y_new_train, X_new_val, Y_new_val, \
+                X[test_idx], Y[test_idx], nb_epoch=args.n_epoch, batch_size=args.batch_size, seed=seed))
+        import pdb;pdb.set_trace()
+        mean = np.mean(results)
+        std = np.std(results)
+        print 'r2 score on %s cross validation: %s (%s)' % (int(args.cross_validation), mean, std)
     import pdb;pdb.set_trace()
 
 if __name__ == '__main__':
