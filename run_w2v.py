d8da226d1de09b4b6fbd37075d9d5b387b981276
hugochan <hugochan2013@gmail.com>
2017-01-28 22:21:45 -0500

    update

--- run_w2v.py
+++ run_w2v.py
@@ -13,7 +13,8 @@ from autoencoder.baseline.word2vec import Word2Vec, save_w2v, load_w2v
 from autoencoder.baseline.doc_word2vec import doc_word2vec
 from autoencoder.utils.io_utils import load_json, dump_json, write_file
 from autoencoder.preprocessing.preprocessing import load_corpus
-from autoencoder.datasets.the20news import CorpusIter20News
+from autoencoder.datasets.reuters import CorpusIterReuters
+# from autoencoder.datasets.the20news import CorpusIter20News
 # from autoencoder.datasets.movie_review_data import CorpusIterMRD
 # from autoencoder.datasets.wiki10plus import CorpusIterWiki10plus
 
@@ -22,9 +23,10 @@ def train(args):
     vocab = load_json(args.vocab)
     # import pdb;pdb.set_trace()
     # load corpus
-    corpus = CorpusIter20News(args.corpus, recursive=True, stem=True, with_docname=False)
-    # corpus = CorpusIterMRD(args.corpus, load_json(args.docnames), stem=True, with_docname=False)
-    # corpus = CorpusIterWiki10plus(args.corpus, load_json(args.docnames), stem=True, with_docname=False)
+    # corpus = CorpusIter20News(args.corpus[0], recursive=True, stem=True, with_docname=False)
+    # corpus = CorpusIterMRD(args.corpus[0], load_json(args.docnames), stem=True, with_docname=False)
+    # corpus = CorpusIterWiki10plus(args.corpus[0], load_json(args.docnames), stem=True, with_docname=False)
+    corpus = CorpusIterReuters(args.corpus, load_json(args.docnames), with_docname=False)
     # print len([1 for x in corpus])
     corpus_iter = lambda: ([word for word in sentence if word in vocab] for sentence in corpus)
     w2v = Word2Vec(args.n_dim, window=args.window_size, \
@@ -34,14 +36,14 @@ def train(args):
     import pdb;pdb.set_trace()
 
 def test(args):
-    corpus = load_corpus(args.corpus)
+    corpus = load_corpus(args.corpus[0])
     docs, vocab_dict = corpus['docs'], corpus['vocab']
     doc_codes = doc_word2vec(docs, revdict(vocab_dict), args.load_model, args.output, size=args.n_dim, avg=True)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--train', action='store_true', help='train flag')
-    parser.add_argument('--corpus', required=True, type=str, help='path to the corpus dir (in training phase) or file (in test phase)')
+    parser.add_argument('--corpus', nargs='*', required=True, type=str, help='path to the corpus dir (in training phase) or file (in test phase)')
     parser.add_argument('-doc', '--docnames', type=str, help='path to the docnames file (in training phase)')
     parser.add_argument('--vocab', required=True, type=str, help='path to the vocab file')
     parser.add_argument('-ne', '--n_epoch', required=True, type=int, help='num of epoches')
