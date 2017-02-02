ced1354d03d5aa1a4710ca9472ddde9071b04f6a
hugochan <hugochan2013@gmail.com>
2017-02-02 14:28:30 -0500

    update

--- corpus2dbnformat.py
+++ corpus2dbnformat.py
@@ -11,7 +11,7 @@ import argparse
 import numpy as np
 
 from autoencoder.preprocessing.preprocessing import load_corpus, doc2vec
-from autoencoder.utils.io_utils import load_json, dump_marshal
+from autoencoder.utils.io_utils import load_json, dump_pickle
 
 def main():
     parser = argparse.ArgumentParser()
@@ -31,26 +31,26 @@ def main():
     out_dir = args.out_dir
     # attributes
     attrs = zip(*sorted(vocab.iteritems(), key=lambda d:[1]))[0]
-    dump_marshal(attrs, os.path.join(out_dir, 'attributes.p'))
-    # import pdb;pdb.set_trace()
+    dump_pickle(attrs, os.path.join(out_dir, 'attributes.p'))
+
     # batches
     bs = args.batch_size
     batches = [bs * (x + 1) for x in range(int(len(docs) / bs) - 1)]
     batches.append(len(docs))
-    dump_marshal(batches, os.path.join(out_dir, 'batches.p'))
+    dump_pickle(batches, os.path.join(out_dir, 'batches.p'))
 
     # bow_batch_x
     for i in range(len(batches)):
-        dump_marshal(X_docs[batches[i - 1] if i > 0 else 0: batches[i]], os.path.join(out_dir, 'bow_batch_%s.p' % batches[i]))
+        dump_pickle(X_docs[batches[i - 1] if i > 0 else 0: batches[i]], os.path.join(out_dir, 'bow_batch_%s.p' % batches[i]))
 
     # # docs_names_batch_x
     # for i in range(len(batches)):
-    #     dump_marshal(doc_names[batches[i - 1] if i > 0 else 0: batches[i]], os.path.join(out_dir, 'docs_names_batch_%s.p' % batches[i]))
+    #     dump_pickle(doc_names[batches[i - 1] if i > 0 else 0: batches[i]], os.path.join(out_dir, 'docs_names_batch_%s.p' % batches[i]))
 
     # class_indices_batch_x
     for i in range(len(batches)):
         data = [doc_labels[doc_names[idx]] for idx in range(batches[i - 1] if i > 0 else 0, batches[i])]
-        dump_marshal(data, os.path.join(out_dir, 'class_indices_batch_%s.p' % batches[i]))
+        dump_pickle(data, os.path.join(out_dir, 'class_indices_batch_%s.p' % batches[i]))
 
     import pdb;pdb.set_trace()
 
--- run_doc2vec.py
+++ run_doc2vec.py
@@ -14,9 +14,9 @@ from gensim.models.doc2vec import TaggedDocument
 from autoencoder.baseline.doc2vec import MyDoc2Vec, save_doc2vec, load_doc2vec, predict
 from autoencoder.utils.io_utils import load_json, dump_json, write_file
 from autoencoder.preprocessing.preprocessing import load_corpus
-from autoencoder.datasets.the20news import CorpusIter20News
+# from autoencoder.datasets.the20news import CorpusIter20News
 # from autoencoder.datasets.movie_review_data import CorpusIterMRD
-# from autoencoder.datasets.wiki10plus import CorpusIterWiki10plus
+from autoencoder.datasets.wiki10plus import CorpusIterWiki10plus
 # from autoencoder.datasets.reuters import CorpusIterReuters
 
 
@@ -24,9 +24,9 @@ def train(args):
     vocab = load_json(args.vocab)
     # import pdb;pdb.set_trace()
     # load corpus
-    corpus = CorpusIter20News(args.corpus[0], recursive=True, stem=True, with_docname=True)
+    # corpus = CorpusIter20News(args.corpus[0], recursive=True, stem=True, with_docname=True)
     # corpus = CorpusIterMRD(args.corpus[0], load_json(args.docnames), stem=True, with_docname=True)
-    # corpus = CorpusIterWiki10plus(args.corpus[0], load_json(args.docnames), stem=True, with_docname=True)
+    corpus = CorpusIterWiki10plus(args.corpus[0], load_json(args.docnames), stem=True, with_docname=True)
     # corpus = CorpusIterReuters(args.corpus, load_json(args.docnames), with_docname=True)
     corpus_iter = lambda: (TaggedDocument([word for word in sentence if word in vocab], tag) for sentence, tag in corpus)
 
@@ -44,11 +44,11 @@ def train(args):
 def test(args):
     vocab = load_json(args.vocab)
     # load corpus
-    corpus = CorpusIter20News(args.corpus[0], recursive=True, stem=True, with_docname=True)
+    # corpus = CorpusIter20News(args.corpus[0], recursive=True, stem=True, with_docname=True)
     # corpus = CorpusIterMRD(args.corpus[0], load_json(args.docnames), stem=True, with_docname=True)
-    # corpus = CorpusIterWiki10plus(args.corpus[0], load_json(args.docnames), stem=True, with_docname=True)
+    corpus = CorpusIterWiki10plus(args.corpus[0], load_json(args.docnames), stem=True, with_docname=True)
     # corpus = CorpusIterReuters(args.corpus, load_json(args.docnames), with_docname=True)
-    corpus_iter = lambda: (TaggedDocument([word for word in sentence if word in vocab], tag) for sentence, tag in corpus)
+    # corpus_iter = lambda: (TaggedDocument([word for word in sentence if word in vocab], tag) for sentence, tag in corpus)
 
     d2v = load_doc2vec(args.load_model)
     doc_codes = predict(d2v, corpus_iter)
@@ -61,16 +61,19 @@ def main():
     parser.add_argument('--corpus', nargs='*', required=True, type=str, help='path to the corpus dir')
     parser.add_argument('-doc', '--docnames', type=str, help='path to the docnames file (in training phase)')
     parser.add_argument('--vocab', required=True, type=str, help='path to the vocab file')
-    parser.add_argument('-ne', '--n_epoch', required=True, type=int, help='num of epoches')
-    parser.add_argument('-nd', '--n_dim', required=True, type=int, help='num of dimensions')
-    parser.add_argument('-ws', '--window_size', required=True, type=int, help='window size')
-    parser.add_argument('-neg', '--negative', required=True, type=int, help='num of negative samples')
+    parser.add_argument('-ne', '--n_epoch', type=int, help='num of epoches')
+    parser.add_argument('-nd', '--n_dim', type=int, help='num of dimensions')
+    parser.add_argument('-ws', '--window_size', type=int, help='window size')
+    parser.add_argument('-neg', '--negative', type=int, help='num of negative samples')
     parser.add_argument('-sm', '--save_model', type=str, default='w2v.mod', help='path to the output model')
     parser.add_argument('-lm', '--load_model', type=str, help='path to the trained model')
     parser.add_argument('-o', '--output', type=str, help='path to the output doc codes file')
     args = parser.parse_args()
 
     if args.train:
+        if not args.n_epoch or not args.n_dim or \
+            not args.window_size or not args.negative:
+            raise 'n_epoch, n_dim, window_size and negative args needed in test phase'
         train(args)
     else:
         if not args.output:
