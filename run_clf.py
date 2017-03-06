427475cddb123c04b58a9079dc75dccf83945584
hugochan <hugochan2013@gmail.com>
2017-03-06 13:23:20 -0500

    update val part

---
+++ run_clf.py
@@ -0,0 +1,82 @@
+'''
+Created on Dec, 2016
+
+@author: hugo
+
+'''
+from __future__ import absolute_import
+import argparse
+import numpy as np
+from keras.utils import np_utils
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import ShuffleSplit
+
+from autoencoder.testing.classifier import multiclass_classifier, multilabel_classifier
+from autoencoder.utils.io_utils import load_json, load_pickle
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('train_doc_codes', type=str, help='path to the train doc codes file')
+    parser.add_argument('train_doc_labels', type=str, help='path to the train doc codes file')
+    parser.add_argument('val_doc_codes', type=str, help='path to the train doc codes file')
+    parser.add_argument('val_doc_labels', type=str, help='path to the train doc labels file')
+    parser.add_argument('test_doc_codes', type=str, help='path to the test doc codes file')
+    parser.add_argument('test_doc_labels', type=str, help='path to the test doc labels file')
+    parser.add_argument('-ne', '--n_epoch', type=int, default=100, help='num of epoches (default 100)')
+    parser.add_argument('-bs', '--batch_size', type=int, default=100, help='batch size (default 100)')
+    parser.add_argument('-mlc', '--multilabel_clf', action='store_true', help='multilabel classification flag')
+
+    args = parser.parse_args()
+
+    # autoencoder
+    train_doc_codes = load_json(args.train_doc_codes)
+    train_doc_labels = load_json(args.train_doc_labels)
+    val_doc_codes = load_json(args.val_doc_codes)
+    val_doc_labels = load_json(args.val_doc_labels)
+    test_doc_codes = load_json(args.test_doc_codes)
+    test_doc_labels = load_json(args.test_doc_labels)
+    X_train = np.r_[train_doc_codes.values()]
+    Y_train = [train_doc_labels[i] for i in train_doc_codes]
+    X_val = np.r_[val_doc_codes.values()]
+    Y_val = [val_doc_labels[i] for i in val_doc_codes]
+    X_test = np.r_[test_doc_codes.values()]
+    Y_test = [test_doc_labels[i] for i in test_doc_codes]
+
+    # # DBN
+    # X_train = np.array(load_pickle(args.train_doc_codes))
+    # Y_train = load_pickle(args.train_doc_labels)
+    # X_val = np.array(load_pickle(args.val_doc_codes))
+    # Y_val = load_pickle(args.val_doc_labels)
+    # X_test = np.array(load_pickle(args.test_doc_codes))
+    # Y_test = load_pickle(args.test_doc_labels)
+
+    if args.multilabel_clf:
+        encoder = MultiLabelBinarizer()
+        encoder.fit(Y_train + Y_val + Y_test)
+        Y_train = encoder.transform(Y_train)
+        Y_val = encoder.transform(Y_val)
+        Y_test = encoder.transform(Y_test)
+    else:
+        encoder = LabelEncoder()
+        encoder.fit(Y_train + Y_val + Y_test)
+        Y_train = np_utils.to_categorical(encoder.transform(Y_train))
+        Y_val = np_utils.to_categorical(encoder.transform(Y_val))
+        Y_test = np_utils.to_categorical(encoder.transform(Y_test))
+
+    seed = 7
+    print 'train: %s, val: %s, test: %s' % (X_train.shape[0], X_val.shape[0], X_test.shape[0])
+    if args.multilabel_clf:
+        results = multilabel_classifier(X_train, Y_train, X_val, Y_val, \
+                X_test, Y_test, nb_epoch=args.n_epoch, batch_size=args.batch_size, seed=seed)
+        print 'f1 score on test set: macro_f1: %s, micro_f1: %s' % tuple(results)
+    else:
+        results = multiclass_classifier(X_train, Y_train, X_val, Y_val, \
+                X_test, Y_test, nb_epoch=args.n_epoch, batch_size=args.batch_size, seed=seed)
+        print 'acc on test set: %s' % results
+
+    import pdb;pdb.set_trace()
+
+if __name__ == '__main__':
+    main()
--- train.py
+++ train.py
@@ -11,7 +11,6 @@ from os import path
 import numpy as np
 
 from autoencoder.core.ae import AutoEncoder, load_model, save_model
-# from autoencoder.core.deepae import DeepAutoEncoder
 from autoencoder.preprocessing.preprocessing import load_corpus, doc2vec, vocab_weights
 from autoencoder.utils.op_utils import vecnorm, add_gaussian_noise, add_masking_noise, add_salt_pepper_noise
 from autoencoder.utils.io_utils import dump_json
@@ -22,51 +21,53 @@ def train(args):
     n_vocab, docs = len(corpus['vocab']), corpus['docs']
     corpus.clear() # save memory
 
+    doc_keys = docs.keys()
     X_docs = []
-    for k in docs.keys():
+    for k in doc_keys:
         X_docs.append(vecnorm(doc2vec(docs[k], n_vocab), 'logmax1', 0))
         del docs[k]
+    X_docs = np.r_[X_docs]
 
-    # replicate the training set for scalability experiments
-    fr = 1.5 - 1
-    size = len(X_docs)
-    X_docs.extend(X_docs[:int(fr * size)])
-    print fr
-
-    np.random.seed(0)
-    np.random.shuffle(X_docs)
     if args.noise == 'gs':
-        X_docs_noisy = add_gaussian_noise(np.r_[X_docs], 0.1)
+        X_docs_noisy = add_gaussian_noise(X_docs, 0.1)
     elif args.noise == 'sp':
-        X_docs_noisy = add_salt_pepper_noise(np.r_[X_docs], 0.1)
+        X_docs_noisy = add_salt_pepper_noise(X_docs, 0.1)
         pass
     elif args.noise == 'mn':
-        X_docs_noisy = add_masking_noise(np.r_[X_docs], 0.01)
+        X_docs_noisy = add_masking_noise(X_docs, 0.01)
     else:
         pass
 
-    n_val = args.n_val
-    # X_train = np.r_[X_docs[:-n_val]]
-    # X_val = np.r_[X_docs[-n_val:]]
-    X_train = np.r_[X_docs[:-n_val]]
-    del X_docs[:-n_val]
-    X_val = np.r_[X_docs]
+    n_samples = X_docs.shape[0]
+    np.random.seed(0)
+    val_idx = np.random.choice(range(n_samples), args.n_val, replace=False)
+    train_idx = list(set(range(n_samples)) - set(val_idx))
+    X_train = X_docs[train_idx]
+    X_val = X_docs[val_idx]
     del X_docs
 
+    # np.random.shuffle(X_docs)
+    # n_val = args.n_val
+    ## X_train = np.r_[X_docs[:-n_val]]
+    ## X_val = np.r_[X_docs[-n_val:]]
+    # X_train = np.r_[X_docs[:-n_val]]
+    # del X_docs[:-n_val]
+    # X_val = np.r_[X_docs]
+    # del X_docs
+
     if args.noise:
-        X_train_noisy = X_docs_noisy[:-n_val]
-        X_val_noisy = X_docs_noisy[-n_val:]
+        # X_train_noisy = X_docs_noisy[:-n_val]
+        # X_val_noisy = X_docs_noisy[-n_val:]
+        X_train_noisy = X_docs_noisy[train_idx]
+        X_val_noisy = X_docs_noisy[val_idx]
         print 'added %s noise' % args.noise
     else:
         X_train_noisy = X_train
         X_val_noisy = X_val
 
-    # model = DeepAutoEncoder
-    model = AutoEncoder
-
     start = timeit.default_timer()
 
-    ae = model(n_vocab, args.n_dim, comp_topk=args.comp_topk, weights_file=args.load_weights)
+    ae = AutoEncoder(n_vocab, args.n_dim, comp_topk=args.comp_topk, weights_file=args.load_weights)
     ae.fit([X_train_noisy, X_train], [X_val_noisy, X_val], nb_epoch=args.n_epoch, \
             batch_size=args.batch_size, feature_weights=None, contractive=args.contractive)
 
@@ -76,10 +77,18 @@ def train(args):
         arch_file  = args.save_model + '.arch'
         weights_file  = args.save_model + '.weights'
         save_model(ae, arch_file, weights_file)
-    print 'Saved model arch and weights file to %s and %s, respectively.' \
+        print 'Saved model arch and weights file to %s and %s, respectively.' \
             % (arch_file, weights_file)
 
-    print 'fr %s' % fr
+    if args.output:
+        train_doc_codes = ae.encoder.predict(X_train)
+        val_doc_codes = ae.encoder.predict(X_val)
+        doc_keys = np.array(doc_keys)
+        dump_json(dict(zip(doc_keys[train_idx].tolist(), train_doc_codes.tolist())), args.output)
+        dump_json(dict(zip(doc_keys[val_idx].tolist(), val_doc_codes.tolist())), args.output + '.val')
+        print 'Saved doc codes file to %s and %s' % (args.output, args.output + '.val')
+
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -93,6 +102,7 @@ def main():
     parser.add_argument('-sm', '--save_model', type=str, default='model', help='path to the output model')
     parser.add_argument('-contr', '--contractive', type=float, help='contractive lambda')
     parser.add_argument('--noise', type=str, help='noise type: gs for Gaussian noise, sp for salt-and-pepper or mn for masking noise')
+    parser.add_argument('-o', '--output', type=str, help='path to the output doc codes file')
     args = parser.parse_args()
 
     if args.noise and not args.noise in ['gs', 'sp', 'mn']:
