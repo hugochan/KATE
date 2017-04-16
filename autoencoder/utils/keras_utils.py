46f530d657920a50f84370d733c0e30494d498a8
hugochan <hugochan2013@gmail.com>
2017-04-16 10:37:47 -0400

    add custom model ckpt

--- autoencoder/utils/keras_utils.py
+++ autoencoder/utils/keras_utils.py
@@ -5,6 +5,7 @@ Created on Nov, 2016
 
 '''
 from __future__ import absolute_import
+import numpy as np
 from keras.layers import Dense
 from keras.callbacks import Callback
 import keras.backend as K
@@ -242,161 +243,100 @@ class Dense_tied(Dense):
             output += self.bias
         return self.activation(output)
 
-# class MyModelCheckpoint(Callback):
-#     '''Save the model after every epoch.
-#     `filepath` can contain named formatting options,
-#     which will be filled the value of `epoch` and
-#     keys in `logs` (passed in `on_epoch_end`).
-#     For example: if `filepath` is `weights.{epoch:02d}-{val_loss:.2f}.hdf5`,
-#     then multiple files will be save with the epoch number and
-#     the validation loss.
-#     # Arguments
-#         filepath: string, path to save the model file.
-#         monitor: quantity to monitor.
-#         verbose: verbosity mode, 0 or 1.
-#         save_best_only: if `save_best_only=True`,
-#             the latest best model according to
-#             the quantity monitored will not be overwritten.
-#         mode: one of {auto, min, max}.
-#             If `save_best_only=True`, the decision
-#             to overwrite the current save file is made
-#             based on either the maximization or the
-#             minimization of the monitored quantity. For `val_acc`,
-#             this should be `max`, for `val_loss` this should
-#             be `min`, etc. In `auto` mode, the direction is
-#             automatically inferred from the name of the monitored quantity.
-#         save_weights_only: if True, then only the model's weights will be
-#             saved (`model.save_weights(filepath)`), else the full model
-#             is saved (`model.save(filepath)`).
-#         period: Interval (number of epochs) between checkpoints.
-#     '''
-#     def __init__(self, filepath, monitor='val_loss', verbose=0,
-#                  save_best_only=False, save_weights_only=False,
-#                  mode='auto', period=1):
-#         super(MyModelCheckpoint, self).__init__()
-#         self.monitor = monitor
-#         self.verbose = verbose
-#         self.filepath = filepath
-#         self.save_best_only = save_best_only
-#         self.save_weights_only = save_weights_only
-#         self.period = period
-#         self.epochs_since_last_save = 0
+class CustomModelCheckpoint(Callback):
+    """Save the model after every epoch.
+    `filepath` can contain named formatting options,
+    which will be filled the value of `epoch` and
+    keys in `logs` (passed in `on_epoch_end`).
+    For example: if `filepath` is `weights.{epoch:02d}-{val_loss:.2f}.hdf5`,
+    then the model checkpoints will be saved with the epoch number and
+    the validation loss in the filename.
+    # Arguments
+        filepath: string, path to save the model file.
+        monitor: quantity to monitor.
+        verbose: verbosity mode, 0 or 1.
+        save_best_only: if `save_best_only=True`,
+            the latest best model according to
+            the quantity monitored will not be overwritten.
+        mode: one of {auto, min, max}.
+            If `save_best_only=True`, the decision
+            to overwrite the current save file is made
+            based on either the maximization or the
+            minimization of the monitored quantity. For `val_acc`,
+            this should be `max`, for `val_loss` this should
+            be `min`, etc. In `auto` mode, the direction is
+            automatically inferred from the name of the monitored quantity.
+        save_weights_only: if True, then only the model's weights will be
+            saved (`model.save_weights(filepath)`), else the full model
+            is saved (`model.save(filepath)`).
+        period: Interval (number of epochs) between checkpoints.
+    """
 
-#         if mode not in ['auto', 'min', 'max']:
-#             warnings.warn('MyModelCheckpoint mode %s is unknown, '
-#                           'fallback to auto mode.' % (mode),
-#                           RuntimeWarning)
-#             mode = 'auto'
+    def __init__(self, custom_model, filepath, monitor='val_loss', verbose=0,
+                 save_best_only=False, save_weights_only=False,
+                 mode='auto', period=1):
+        super(CustomModelCheckpoint, self).__init__()
+        self.custom_model = custom_model
+        self.monitor = monitor
+        self.verbose = verbose
+        self.filepath = filepath
+        self.save_best_only = save_best_only
+        self.save_weights_only = save_weights_only
+        self.period = period
+        self.epochs_since_last_save = 0
 
-#         if mode == 'min':
-#             self.monitor_op = np.less
-#             self.best = np.Inf
-#         elif mode == 'max':
-#             self.monitor_op = np.greater
-#             self.best = -np.Inf
-#         else:
-#             if 'acc' in self.monitor:
-#                 self.monitor_op = np.greater
-#                 self.best = -np.Inf
-#             else:
-#                 self.monitor_op = np.less
-#                 self.best = np.Inf
+        if mode not in ['auto', 'min', 'max']:
+            warnings.warn('CustomModelCheckpoint mode %s is unknown, '
+                          'fallback to auto mode.' % (mode),
+                          RuntimeWarning)
+            mode = 'auto'
 
-#     def on_epoch_end(self, epoch, logs={}):
-#         self.epochs_since_last_save += 1
-#         if self.epochs_since_last_save >= self.period:
-#             self.epochs_since_last_save = 0
-#             filepath = self.filepath.format(epoch=epoch, **logs)
-#             if self.save_best_only:
-#                 current = logs.get(self.monitor)
-#                 if current is None:
-#                     warnings.warn('Can save best model only with %s available, '
-#                                   'skipping.' % (self.monitor), RuntimeWarning)
-#                 else:
-#                     if self.monitor_op(current, self.best):
-#                         if self.verbose > 0:
-#                             print('Epoch %05d: %s improved from %0.5f to %0.5f,'
-#                                   ' saving model to %s'
-#                                   % (epoch, self.monitor, self.best,
-#                                      current, filepath))
-#                         self.best = current
-#                         if self.save_weights_only:
-#                             self.model.save_weights(filepath, overwrite=True)
-#                         else:
-#                             self.model.save(filepath, overwrite=True)
-#                     else:
-#                         if self.verbose > 0:
-#                             print('Epoch %05d: %s did not improve' %
-#                                   (epoch, self.monitor))
-#             else:
-#                 if self.verbose > 0:
-#                     print('Epoch %05d: saving model to %s' % (epoch, filepath))
-#                 if self.save_weights_only:
-#                     self.model.save_weights(filepath, overwrite=True)
-#                 else:
-#                     self.model.save(filepath, overwrite=True)
+        if mode == 'min':
+            self.monitor_op = np.less
+            self.best = np.Inf
+        elif mode == 'max':
+            self.monitor_op = np.greater
+            self.best = -np.Inf
+        else:
+            if 'acc' in self.monitor or self.monitor.startswith('fmeasure'):
+                self.monitor_op = np.greater
+                self.best = -np.Inf
+            else:
+                self.monitor_op = np.less
+                self.best = np.Inf
 
-# class KSparseScheduler(Callback):
-#     '''Callback that schedules the sparsity level over epochs as follows:
-#     Suppose we are aiming for a sparsity level of k = 15.
-#     Then, we start off with a large sparsity level (e.g. k = 100)
-#     for which the k-sparse autoencoder can train all the hidden units.
-#     We then linearly decrease the sparsity level from k = 100 to k = 15
-#     over the first half of the epochs.
-#     This initializes the autoencoder in a good regime,
-#     for which all of the hidden units have a significant chance of being picked.
-#     Then, we keep k = 15 for the second half of the epochs.
-#     With this scheduling, we can train all of the filters, even for low sparsity levels.
-
-#     ref: Makhzani, Alireza, and Brendan Frey. "k-Sparse Autoencoders." arXiv preprint arXiv:1312.5663 (2013).
-
-#     Here, we adopt a slightly different strategy. We start from a starting sparsity level,
-#     and then decrease in every epoch until we hit the ending sparsity level.
-#     Note that when testing, we use a different sparsity level (alpha * end_k).
-
-#     Parameters
-#     ----------
-#     topk_dict : sparsity level
-#     start_k : starting sparsity level
-#     end_k : ending sparsity level
-#     step_k : step size of decreasing sparsity level
-#     alpha : use alpha * end_k at testing time
-#     '''
-
-#     def __init__(self, topk_dict, start_k, end_k, step_k, alpha, test):
-#         self.topk_dict = topk_dict
-#         self.key = topk_dict.keys()[0]
-#         self.start_k = start_k
-#         self.end_k = end_k
-#         self.step_k = step_k
-#         self.alpha = alpha
-#         self.test = test
-#         super(KSparseScheduler, self).__init__()
-
-#     def on_train_begin(self, logs={}):
-#         # import pdb;pdb.set_trace()
-#         # self.topk_dict[self.key] = int(self.start_k)
-#         self.topk_dict[self.key].assign(int(self.start_k))
-#         # self.topk_dict[self.key] = tf.mul(tf.constant(1, tf.int32), self.start_k)
-#         # print 'on_train_begin'
-#         # print self.topk_dict[self.key]
-
-#     def on_train_end(self, logs={}):
-#         # self.topk_dict[self.key] = int(self.alpha * self.end_k)
-#         self.topk_dict[self.key] = tf.cast(self.topk_dict[self.key], tf.float32)
-#         self.topk_dict[self.key] = tf.mul(self.topk_dict[self.key], self.alpha)
-#         self.topk_dict[self.key] = tf.cast(self.topk_dict[self.key], tf.int32)
-#         # print 'on_train_end'
-#         # print self.topk_dict
-
-#     def on_epoch_end(self, epoch, logs={}):
-#         # self.topk_dict[self.key] = max(self.topk_dict[self.key] - self.step_k, self.end_k)
-#         # prev = self.topk_dict[self.key]
-#         self.topk_dict[self.key] = tf.maximum(self.topk_dict[self.key] - self.step_k, self.end_k)
-#         # with tf.Session() as sess:
-#         #     init = tf.global_variables_initializer()
-#         #     sess.run(init)
-#         #     import pdb;pdb.set_trace()
-#         #     print sess.run(self.topk_dict[self.key])
-#         # print 'on_epoch_end'
-#         # print self.topk_dict
+    def on_epoch_end(self, epoch, logs=None):
+        model = self.custom_model
+        logs = logs or {}
+        self.epochs_since_last_save += 1
+        if self.epochs_since_last_save >= self.period:
+            self.epochs_since_last_save = 0
+            filepath = self.filepath.format(epoch=epoch, **logs)
+            if self.save_best_only:
+                current = logs.get(self.monitor)
+                if current is None:
+                    warnings.warn('Can save best model only with %s available, '
+                                  'skipping.' % (self.monitor), RuntimeWarning)
+                else:
+                    if self.monitor_op(current, self.best):
+                        if self.verbose > 0:
+                            print('Epoch %05d: %s improved from %0.5f to %0.5f,'
+                                  ' saving model to %s'
+                                  % (epoch, self.monitor, self.best,
+                                     current, filepath))
+                        self.best = current
+                        if self.save_weights_only:
+                            model.save_weights(filepath, overwrite=True)
+                        else:
+                            model.save(filepath, overwrite=True)
+                    else:
+                        if self.verbose > 0:
+                            print('Epoch %05d: %s did not improve' %
+                                  (epoch, self.monitor))
+            else:
+                if self.verbose > 0:
+                    print('Epoch %05d: saving model to %s' % (epoch, filepath))
+                if self.save_weights_only:
+                    model.save_weights(filepath, overwrite=True)
+                else:
+                    model.save(filepath, overwrite=True)
