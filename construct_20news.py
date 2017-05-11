624639862606b14170bdc72ba97a83197998a341
hugochan <hugochan2013@gmail.com>
2017-05-11 00:19:55 -0400

    rewrite

--- construct_20news.py
+++ construct_20news.py
@@ -6,24 +6,25 @@ Created on Dec, 2016
 '''
 
 import os
-import sys
+import argparse
+
 from autoencoder.preprocessing.preprocessing import construct_train_test_corpus, generate_20news_doc_labels#, generate_8k_doc_labels
 
 
 def main():
-    usage = 'python construct_20news.py [train_path] [test_path] [out_path]'
-    try:
-        train_path = sys.argv[1]
-        test_path = sys.argv[2]
-        out_path = sys.argv[3]
-    except:
-        print usage
-        sys.exit()
-    train_corpus, test_corpus = construct_train_test_corpus(train_path, test_path, out_path, threshold=5, topn=2000)
-    train_labels = generate_20news_doc_labels(train_corpus['docs'].keys(), os.path.join(out_path, 'train.labels'))
-    test_labels = generate_20news_doc_labels(test_corpus['docs'].keys(), os.path.join(out_path, 'test.labels'))
-    # train_labels = generate_8k_doc_labels(train_corpus['docs'].keys(), os.path.join(out_path, 'train.labels'))
-    # test_labels = generate_8k_doc_labels(test_corpus['docs'].keys(), os.path.join(out_path, 'test.labels'))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-train', '--train_path', type=str, required=True, help='path to the training corpus')
+    parser.add_argument('-test', '--test_path', type=str, required=True, help='path to the test corpus')
+    parser.add_argument('-o', '--out_dir', type=str, required=True, help='path to the output dir')
+    parser.add_argument('-threshold', '--threshold', type=int, default=5, help='word frequency threshold (default 5)')
+    parser.add_argument('-topn', '--topn', type=int, default=2000, help='top n words (default 2000)')
+    args = parser.parse_args()
+
+    train_corpus, test_corpus = construct_train_test_corpus(args.train_path, args.test_path, args.out_dir, threshold=args.threshold, topn=args.topn)
+    train_labels = generate_20news_doc_labels(train_corpus['docs'].keys(), os.path.join(args.out_dir, 'train.labels'))
+    test_labels = generate_20news_doc_labels(test_corpus['docs'].keys(), os.path.join(args.out_dir, 'test.labels'))
+    # train_labels = generate_8k_doc_labels(train_corpus['docs'].keys(), os.path.join(args.out_dir, 'train.labels'))
+    # test_labels = generate_8k_doc_labels(test_corpus['docs'].keys(), os.path.join(args.out_dir, 'test.labels'))
 
 if __name__ == "__main__":
     main()
